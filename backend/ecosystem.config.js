1edfddb9f1529ad0584adc4be2a48722753dfee5
Actiq <hi@actiq.xyz>
2026-02-09 03:45:13 +0300

    Fix PORT in ecosystem config

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
     max_memory_restart: '1G',
     env: {
       NODE_ENV: 'production',
-      PORT: 3000
+      PORT: 4000
     },
     error_file: './logs/error.log',
     out_file: './logs/output.log',
